6ae57888ea9e9d31a997dd3a30ec7e7333cccfb0
laojia <151761739@qq.com>
2023-06-29 15:18:17 +0800

    add my folder

---
+++ my/my.go
@@ -0,0 +1,14 @@
+package my
+
+/**
+* @Author: jack.walker
+* @File: my.go
+* @CreateDate: 2023/6/29 15:15
+* @ChangeDate: 2023/6/29 15:15
+* @Version: 1.0.0
+* @Description:
+ */
+
+func SayHi() string {
+	return "yes, it's laojia's repo!"
+}
