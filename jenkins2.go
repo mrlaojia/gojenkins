c352714d40ce6817963359e9606e75be1cd8f6e2
laojia <151761739@qq.com>
2023-06-30 08:17:37 +0800

    add sub view operation

---
+++ jenkins2.go
@@ -0,0 +1,97 @@
+package gojenkins
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+/**
+* @Author: jack.walker
+* @File: jenkins2.go
+* @CreateDate: 2023/6/29 16:45
+* @ChangeDate: 2023/6/29 16:45
+* @Version: 1.0.0
+* @Description: 对原库的扩充
+ */
+
+// CreateViewInFolder 在某个文件夹下创建 view
+// parents 可以指定嵌套深度，包括顶层，可以替代 j.CreateView
+func (j *Jenkins) CreateViewInFolder(ctx context.Context, viewName string, viewType string, parents ...string) (*View, error) {
+
+	baseur := j.depJobUrl(parents...)
+	view := &View{Jenkins: j, Raw: new(ViewResponse), Base: baseur + "/view/" + viewName}
+
+	endpoint := baseur + "/createView"
+	data := map[string]string{
+		"name":   viewName,
+		"mode":   viewType,
+		"Submit": "OK",
+		"json": makeJson(map[string]string{
+			"name": viewName,
+			"mode": viewType,
+		}),
+	}
+	r, err := j.Requester.Post(ctx, endpoint, nil, view.Raw, data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if r.StatusCode == 200 {
+		return j.GetSubView(ctx, viewName, parents...)
+	}
+	return nil, errors.New(strconv.Itoa(r.StatusCode))
+}
+
+// GetSubView 查看某个文件夹下的某个view
+// parents 可以指定嵌套深度，包括顶层，可以替代 j.GetView
+func (j *Jenkins) GetSubView(ctx context.Context, name string, parents ...string) (*View, error) {
+	url := j.depJobUrl(parents...) + "/view/" + name
+	view := View{Jenkins: j, Raw: new(ViewResponse), Base: url}
+	_, err := view.Poll(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &view, nil
+}
+
+// GetAllSubViews 查看某个文件夹下的所有view
+// parents 可以指定嵌套深度，包括顶层，可以替代 j.GetAllViews
+func (j *Jenkins) GetAllSubViews(ctx context.Context, parents ...string) ([]*View, error) {
+	_, err := j.Poll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	jenkinsRaw := new(ExecutorResponse)
+	rsp, err := j.Requester.GetJSON(ctx, j.depJobUrl(parents...), jenkinsRaw, nil)
+	if j.Raw == nil || rsp.StatusCode != http.StatusOK {
+		return nil, errors.New("Connection Failed, Please verify that the host and credentials are correct.")
+	}
+
+	views := make([]*View, len(jenkinsRaw.Views))
+	for i, v := range jenkinsRaw.Views {
+		views[i], _ = j.GetSubView(ctx, v.Name, parents...)
+	}
+	return views, nil
+}
+
+// depUrl 组合成 /job/x/job/x 格式的url
+func (j *Jenkins) depJobUrl(parents ...string) string {
+	return j.depUrl("job", parents...)
+}
+
+// depUrl 组合成 url
+// tag: job, view ...
+// parents: 层级关系
+func (j *Jenkins) depUrl(tag string, parents ...string) string {
+	var base string
+	for _, p := range parents {
+		base += fmt.Sprintf("/%s/%s", tag, p)
+	}
+
+	return base
+}
---
+++ jenkins2_test.go
@@ -0,0 +1,56 @@
+package gojenkins
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+/**
+* @Author: jack.walker
+* @File: jenkins2_test.go
+* @CreateDate: 2023/6/29 16:55
+* @ChangeDate: 2023/6/29 16:55
+* @Version: 1.0.0
+* @Description: test for jenkins2
+ */
+
+func getTestJenkins() (*Jenkins, error) {
+	ctx := context.Background()
+	jenkins = CreateJenkins(nil, "http://jenkins.ingress.local/", "admin", "hello123")
+	return jenkins.Init(ctx)
+}
+
+func TestJenkins_CreateViewInFolder(t *testing.T) {
+	ctx := context.Background()
+	jenkins, err := getTestJenkins()
+	assert.Nil(t, err)
+
+	viewName := "dev_pro3"
+	view1, err := jenkins.CreateViewInFolder(ctx, viewName, LIST_VIEW, "dev", "dev01")
+	assert.Nil(t, err)
+	assert.Equal(t, viewName, view1.GetName())
+}
+
+func TestJenkins_GetSubView(t *testing.T) {
+	ctx := context.Background()
+	jenkins, err := getTestJenkins()
+	assert.Nil(t, err)
+
+	viewName := "dev_pro2"
+	v, err := jenkins.GetSubView(ctx, viewName, "dev", "dev01")
+	assert.Nil(t, err)
+	assert.Equal(t, v.GetName(), viewName)
+}
+
+func TestJenkins_GetAllSubViews(t *testing.T) {
+	ctx := context.Background()
+	jenkins, err := getTestJenkins()
+	assert.Nil(t, err)
+
+	vs, err := jenkins.GetAllSubViews(ctx, "dev", "dev01")
+	assert.Nil(t, err)
+	for _, r := range vs {
+		t.Log(r.GetUrl(), r.GetName())
+	}
+}
